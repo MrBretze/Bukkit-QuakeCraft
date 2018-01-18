7cdea8894df1c91b1637f9a1bc243dbdacd57de9
MrBretzel <axelolo68@gmail.com>
2018-01-18 19:36:24 +0200

    Up

--- src/main/java/fr/bretzel/quake/game/task/ReloadTask.java
+++ src/main/java/fr/bretzel/quake/game/task/ReloadTask.java
@@ -16,11 +16,11 @@
  */
 package fr.bretzel.quake.game.task;
 
-import fr.bretzel.quake.Quake;
 import fr.bretzel.quake.SchootTask;
 import fr.bretzel.quake.PlayerInfo;
 import fr.bretzel.quake.Title;
 import org.bukkit.ChatColor;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -28,5 +28,65 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class ReloadTask extends SchootTask {
 
+    private int id = -1;
+    private JavaPlugin javaPlugin;
 
+    private double reload_time = 0;
+
+    private String bare = "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||".trim();
+
+    private boolean isDash = false;
+
+    private double remplissage = 0;
+
+    public ReloadTask(JavaPlugin javaPlugin, PlayerInfo info, double reload_time, boolean isDash) {
+        super(info);
+        this.reload_time = reload_time;
+        this.isDash = isDash;
+
+        Title.sendTitle(info.getPlayer(), 0, 20, 0);
+
+        this.javaPlugin = javaPlugin;
+        this.id = javaPlugin.getServer().getScheduler().scheduleSyncRepeatingTask(javaPlugin, this, 0, 0);
+    }
+
+    @Override
+    public void run() {
+        StringBuilder builder = new StringBuilder(bare);
+
+        remplissage += 5 / reload_time;
+        double position = Math.round(bare.length() * remplissage / 100);
+
+        if (remplissage >= 100) {
+            if(isDash) {
+                getInfo().setDash(true);
+            } else {
+                getInfo().setShoot(true);
+            }
+            cancel();
+        }
+
+        if (position <= bare.length())
+            builder.insert((int) position, ChatColor.GRAY + "" + ChatColor.BOLD);
+
+        if (!isDash) {
+            Title.sendTitle(getInfo().getPlayer(), Title.Type.ACTIONBAR,  ChatColor.GOLD + "" + "Reloading shoot: " + ChatColor.DARK_RED + "[" + ChatColor.GREEN + builder.toString() + ChatColor.DARK_RED + "]");
+        } else {
+            ItemMeta meta = getInfo().getPlayer().getInventory().getItemInMainHand().getItemMeta();
+            meta.setDisplayName(ChatColor.GREEN + builder.toString());
+            //getInfo().getPlayer().getInventory().getItemInMainHand().setItemMeta(meta);
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public JavaPlugin getJavaPlugin() {
+        return javaPlugin;
+    }
+
+    public void cancel() {
+        getJavaPlugin().getServer().getScheduler().cancelTask(getId());
+    }
 }
